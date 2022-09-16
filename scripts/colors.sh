6220f35e03b8f56498ace53f49f94aede81ba0d8
acarlson <34431218+acarlson99@users.noreply.github.com>
2022-09-15 17:52:13 -0700

    Update colors.sh

--- scripts/colors.sh
+++ scripts/colors.sh
@@ -1,4 +1,4 @@
-# curl URL | sh -l
+# curl https://raw.githubusercontent.com/acarlson99/acarlson99.github.io/master/scripts/colors.sh | bash -l
 P=(  █ ░ ▒ ▓)
 while :;do
 printf "\e[9$(( ( RANDOM % 7 )  + 1 ))m\e[$[RANDOM%$LINES+1];$[RANDOM%$COLUMNS+1]f${P[$RANDOM%5]}"
