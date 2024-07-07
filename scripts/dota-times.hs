3a2f99a8954bd2677ab96ab0ffa5c1e21722f52d
alexander <a@a>
2024-07-06 17:09:14 -0700

    lint

--- scripts/dota-times.hs
+++ scripts/dota-times.hs
@@ -1,6 +1,7 @@
 -- generate table of dota2 timings
 
-import Control.Exception
+module Main where
+
 import Control.Monad
 import System.Environment
 import Text.Printf
@@ -8,24 +9,25 @@ import Text.Read
 
 asciiGen :: Int -> [(String, [Bool])] -> IO ()
 asciiGen maxMin lts = do
-  (printFirstCol "") <> (printCols $ map show [0 .. maxMin]) <> printf "\n"
-  (mapM_ (\(a, b) -> (printFirstCol a) <> (printCols $ map (\p -> if p then "XX" else "  ") b) <> printf "\n") lts)
+  printFirstCol "" <> printCols (map show [0 .. maxMin]) <> printf "\n"
+  mapM_ (\(a, b) -> printFirstCol a <> printCols (map (\p -> if p then "XX" else "  ") b) <> printf "\n") lts
   where
-    maxLen = foldr1 max $ map (length . fst) lts
+    maxLen = maximum $ map (length . fst) lts
     printFirstCol = printf "%*s --" maxLen
     printCols = foldr ((<>) . printf " %2s |") (return ())
 
 htmlGen :: Int -> [(String, [Bool])] -> IO ()
 htmlGen maxMin lts = do
   printf "<table>"
-  (printFirstCol "") <> (printCols1 $ map show [0 .. maxMin]) <> printf "\n"
-  (printf "<tr>") <> (mapM_ (\(a, b) -> (printFirstCol a) <> (printCols2 b) <> printf "</tr>\n") lts)
+  printFirstCol "" <> printCols1 (map show [0 .. maxMin]) <> printf "\n"
+  printf "<tr>" <> mapM_ (\(a, b) -> printFirstCol a <> printCols2 b <> printf "</tr>\n") lts
   printf "<table>\n"
   where
     printFirstCol = printf "<td>%s</td>"
     printCols1 = foldr ((<>) . printf "<td>%s</td>") (return ())
-    printCols2 = foldr ((<>) . (\p -> (printf "<td ") <> (printf (if p then "style='background-color: green; color: green;'" else "style='opacity:0;'")) <> (printf ">XX</td>"))) (return ())
+    printCols2 = foldr ((<>) . (\p -> printf "<td " <> printf (if p then "style='background-color: green; color: green;'" else "style='opacity:0;'") <> (printf ">XX</td>"))) (return ())
 
+usage :: String
 usage = "Usage: runhaskell dota-times.hs [MODE] [MIN]\n\tMODE: \"html\" or \"ascii\"\n\t MIN: max time to display"
 
 main :: IO ()
@@ -43,6 +45,7 @@ main = do
     printTabsArg "ascii" = printTabs asciiGen
     printTabsArg _ = printf "%s\n" usage
 
+printTabs :: (Num a, Ord a, Enum a) => (a -> [(String, [Bool])] -> b) -> a -> b
 printTabs fn maxMin = fn maxMin lts
   where
     lists =
@@ -52,5 +55,4 @@ printTabs fn maxMin = fn maxMin lts
               ("water", [2, 4]),
               ("power", [6, 8 ..])
             ]
-    containedInList n = not . notElem n
-    lts = map (liftM2 (,) fst (traverse containedInList [0 .. maxMin] . snd)) lists
+    lts = map (liftM2 (,) fst (traverse elem [0 .. maxMin] . snd)) lists
