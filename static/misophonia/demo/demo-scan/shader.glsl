9274df18e77f55adc1c123927a24c514d48e8d90
Alexander <bombblob@gmail.com>
2025-05-03 10:52:16 -0700

    update demo shaders

--- static/misophonia/demo/demo-scan/shader.glsl
+++ static/misophonia/demo/demo-scan/shader.glsl
@@ -12,6 +12,7 @@ uniform float u_time;
 uniform vec2 u_resolution;
 uniform sampler2D u_texture0;
 uniform sampler2D u_texture1;
+uniform sampler2D u_texture2;
 
 #define PI 3.141592653589
 
@@ -31,10 +32,18 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     // Time varying pixel color
     vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
 
-    vec3 tx = texture(iChannel0, uv).rgb;
+    vec3 tx = texture(u_texture0, uv).rgb;
 
-    //col = tx;
+    vec3 a = texture(u_texture1, uv).rgb;
+    vec3 b = texture(u_texture2, uv).rgb;
+
+    // TODO: handle non-monochrome case maybe
+    col = mix(a,b,tx);
+
+#if 0
+    col = tx;
     col *= sin(uv.y*PI*.2+length(tx)*PI*1.+iTime)*.5+.5;
+#endif
 
     // Output to screen
     fragColor = vec4(col,1.);
